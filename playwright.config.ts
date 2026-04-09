1cb2eae86280e06cfb4aae5e5559eeab239987f2
matias-arana_cvideo <matias.arana@nubiral.onmicrosoft.com>
2026-04-09 20:38:56 -0300

    se agrego configuracion en playwright.config para generar el reporte html

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './src/tests',
   timeout: 30000,
+  reporter: 'html',  // <-- esto genera la carpeta playwright-report/
   use: {
-    baseURL: process.env.BASE_URL || 'https://api.example.com',
+    //baseURL: process.env.BASE_URL || 'https://api.example.com',
     extraHTTPHeaders: {
       'Accept': 'application/json',
     },
