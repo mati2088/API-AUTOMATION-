5a99af30f0be2aa081d827f620df3d6ca775646d
matias-arana_cvideo <matias.arana@nubiral.onmicrosoft.com>
2026-04-09 20:29:11 -0300

    se modifico cuenta de cliente

--- src/tests/cliente.spec.ts
+++ src/tests/cliente.spec.ts
@@ -21,7 +21,7 @@ test("validar personalInfo", async({request})=>{
     console.log(body)
     //validaciones
     expect(body.data.personalInfo.name).toBe("Juan Pérez");
-    expect(body.data.accounts[0].id).toBe("ACC-P2CFX")
+    expect(body.data.accounts[0].id).toBe("ACC-PIPILINE")
     //expect(body.data.cards[0].id).toBe("CARD001")
 
     expect(body.data.personalInfo.address).not.toBe(null)
